f909aa02927a9ca1adddf0dc20be3fcb6a3547ff
dangbinz <46754956+dangbinz@users.noreply.github.com>
2021-09-15 10:27:38 +0700

    Initial commit

---
+++ src/test/java/com/example/demo2/Springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2.Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
